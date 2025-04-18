e6a1e869b94bac664b546c969b6493b968105fe7
ZhangWei0130 <1195123240@qq.com>
2025-04-18 08:51:27 +0800

    fix infer

--- tools/infer_mm.py
+++ tools/infer_mm.py
@@ -19,21 +19,17 @@ from PIL import Image, ImageDraw, ImageFont
 
 class SemSeg:
     def __init__(self, cfg) -> None:
-        # inference device cuda or cpu
         self.device = torch.device(cfg['DEVICE'])
 
-        # get dataset classes' colors and labels
         self.palette = eval(cfg['DATASET']['NAME']).PALETTE
         self.labels = eval(cfg['DATASET']['NAME']).CLASSES
 
-        # initialize the model and load weights and send to device
         self.model = eval(cfg['MODEL']['NAME'])(cfg['MODEL']['BACKBONE'], len(self.palette), cfg['DATASET']['MODALS'])
         msg = self.model.load_state_dict(torch.load(cfg['EVAL']['MODEL_PATH'], map_location='cpu'))
         print(msg)
         self.model = self.model.to(self.device)
         self.model.eval()
 
-        # preprocess parameters and transformation pipeline
         self.size = cfg['TEST']['IMAGE_SIZE']
         self.tf_pipeline_img = T.Compose([
             T.Resize(self.size),
@@ -49,14 +45,19 @@ class SemSeg:
 
     def postprocess(self, orig_img: Tensor, seg_map: Tensor, overlay: bool) -> Tensor:
         seg_map = seg_map.softmax(dim=1).argmax(dim=1).cpu().to(int)
-
         seg_image = self.palette[seg_map].squeeze()
         if overlay: 
             seg_image = (orig_img.permute(1, 2, 0) * 0.4) + (seg_image * 0.6)
-
         image = seg_image.to(torch.uint8)
-        pil_image = Image.fromarray(image.numpy())
-        return pil_image
+        return Image.fromarray(image.numpy())
+
+    def postprocess_label(self, orig_img: Tensor, label: Tensor, overlay: bool) -> Tensor:
+        label = label.squeeze().cpu().to(int)
+        label_image = self.palette[label].squeeze()
+        if overlay:
+            label_image = (orig_img.permute(1, 2, 0) * 0.4) + (label_image * 0.6)
+        image = label_image.to(torch.uint8)
+        return Image.fromarray(image.numpy())
 
     @torch.inference_mode()
     @timer
@@ -72,7 +73,7 @@ class SemSeg:
             img = img.repeat(3, 1, 1)
         return img
 
-    def predict(self, img_fname: str, overlay: bool) -> Tensor:
+    def predict(self, img_fname: str, overlay: bool) -> tuple:
         if cfg['DATASET']['NAME'] == 'DELIVER':
             x1 = img_fname.replace('/img', '/hha').replace('_rgb', '_depth')
             x2 = img_fname.replace('/img', '/lidar').replace('_rgb', '_lidar')
@@ -88,22 +89,28 @@ class SemSeg:
 
         image = io.read_image(img_fname)[:3, ...]
         img = self.tf_pipeline_img(image).to(self.device)
-        # --- modals
+
         x1 = self._open_img(x1)
         x1 = self.tf_pipeline_modal(x1).to(self.device)
         x2 = self._open_img(x2)
         x2 = self.tf_pipeline_modal(x2).to(self.device)
         x3 = self._open_img(x3)
         x3 = self.tf_pipeline_modal(x3).to(self.device)
-        label = io.read_image(lbl_path)[0,...].unsqueeze(0)
-        label[label==255] = 0
+
+        label = io.read_image(lbl_path)
+        label = label[0, ...].unsqueeze(0)
+        label = T.Resize(self.size, interpolation=T.InterpolationMode.NEAREST)(label)
+        label[label == 255] = 0
         label -= 1
 
         sample = [img, x1, x2, x3][:len(modals)]
 
-        seg_map = self.model_forward(sample)
-        seg_map = self.postprocess(image, seg_map, overlay)
-        return seg_map
+        seg_map = self.model_forward(sample)[-1]
+        
+        return (
+            self.postprocess(image, seg_map, overlay),
+            self.postprocess_label(image, label, overlay)
+        )
 
 
 if __name__ == '__main__':
@@ -113,17 +120,12 @@ if __name__ == '__main__':
     with open(args.cfg) as f:
         cfg = yaml.load(f, Loader=yaml.SafeLoader)
 
-    # cases = ['cloud', 'fog', 'night', 'rain', 'sun', 'motionblur', 'overexposure', 'underexposure', 'lidarjitter', 'eventlowres', None]
     cases = ['lidarjitter']
-
     modals = cfg['DATASET']['MODALS']
 
     test_file = Path(cfg['TEST']['FILE'])
     if not test_file.exists():
         raise FileNotFoundError(test_file)
-
-    # print(f"Model {cfg['MODEL']['NAME']} {cfg['MODEL']['BACKBONE']}")
-    # print(f"Model {cfg['DATASET']['NAME']}")
 
     modals_name = ''.join([m[0] for m in cfg['DATASET']['MODALS']])
     save_dir = Path(cfg['SAVE_DIR']) / 'test_results' / (cfg['DATASET']['NAME']+'_'+cfg['MODEL']['BACKBONE']+'_'+modals_name)
@@ -131,11 +133,14 @@ if __name__ == '__main__':
     semseg = SemSeg(cfg)
 
     if test_file.is_file():
-        segmap = semseg.predict(str(test_file), cfg['TEST']['OVERLAY'])
-        segmap.save(save_dir / f"{str(test_file.stem)}.png")
+        segmap, labelmap = semseg.predict(str(test_file), cfg['TEST']['OVERLAY'])
+        (save_dir/'predictions').mkdir(parents=True, exist_ok=True)
+        (save_dir/'labels').mkdir(parents=True, exist_ok=True)
+        segmap.save(save_dir/'predictions'/f"{test_file.stem}.png")
+        labelmap.save(save_dir/'labels'/f"{test_file.stem}.png")
     else:
         if cfg['DATASET']['NAME'] == 'DELIVER':
-            files = sorted(glob.glob(os.path.join(*[str(test_file), 'img', '*', 'val', '*', '*.png']))) # --- Deliver
+            files = sorted(glob.glob(os.path.join(*[str(test_file), 'img', '*', 'val', '*', '*.png'])))
         elif cfg['DATASET']['NAME'] == 'KITTI360':
             source = os.path.join(test_file, 'val.txt')
             files = []
@@ -144,17 +149,21 @@ if __name__ == '__main__':
             for item in files_:
                 file_name = item.strip()
                 if ' ' in file_name:
-                    # --- KITTI-360
                     file_name = os.path.join(*[str(test_file), file_name.split(' ')[0]])
                 files.append(file_name)
         else:
             raise NotImplementedError()
 
         for file in files:
-            print(file)
-            if not '2013_05_28_drive_0000_sync' in file:
-                continue
-            segmap = semseg.predict(file, cfg['TEST']['OVERLAY'])
-            save_path = os.path.join(str(save_dir),file)
-            os.makedirs(os.path.dirname(save_path), exist_ok=True)
-            segmap.save(save_path)
+            segmap, labelmap = semseg.predict(file, cfg['TEST']['OVERLAY'])
+            relative_path = Path(os.path.relpath(file, test_file))
+            pred_save_path = save_dir/'predictions'/relative_path
+            pred_save_path.parent.mkdir(parents=True, exist_ok=True)
+            segmap.save(pred_save_path)
+            
+            label_save_path = save_dir/'labels'/relative_path
+            label_save_path.parent.mkdir(parents=True, exist_ok=True)
+            labelmap.save(label_save_path)
+            
+            print(f"Prediction saved: {pred_save_path}")
+            print(f"Label saved: {label_save_path}")
